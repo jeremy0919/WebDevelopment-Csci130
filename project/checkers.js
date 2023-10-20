c0e174a2ab8adc41b425a71850f36f8f7d7582ce
jeremy0919 <jeremy0919@icloud.com>
2023-10-20 13:32:02 -0700

    Update checkers.js

--- project/checkers.js
+++ project/checkers.js
@@ -1,37 +1,69 @@
 function createTable(){
    // const body = document.body,
     table = document.createElement('table');
-    table.style.width = '100px';
+   // table.style.width = '100px';
+    
     table.style.borderCollapse = "collapse";
+    table.style.width = "400px";
     let n = 10;
     let tablebody = document.createElement("tbody");
     table.appendChild(tablebody);
-  //  table.style.c
-    let x = 1;
-    for(let i =1; i<=n;++i){
-        x = x*i;
+
+    for(let i =0; i<n;++i){
+      
         let tr = document.createElement("trow");
-        tablebody.appendChild(tr); 
-        for(let j =0; j<=n; ++j){
+      //  tablebody.appendChild(tr); 
+        for(let j =0; j<n; ++j){
             let td = document.createElement("td");
         //    let td2= document.createElement("td");
-            let input = document.createElement("INPUT");
-            input.setAttribute("type", "text");
-            let input2 = document.createElement("INPUT");
-            input2.setAttribute("type", "text");
-            if(j ==0){
-            input.setAttribute("value",i );
-            }
-            if(j == 1){
-                input2.setAttribute("value", x );
-            }
+            let input = document.createElement("canvas");
+    
+            let input2 = document.createElement("canvas");
+          //  input2.setAttribute("input","text");
+            input2.setAttribute("height", "40");
+            input2.setAttribute("width", "40");
+            input.setAttribute("height", "40");
+       
+            input.setAttribute("width", "40");
+            
+            if(i%2==0){
+                if(j%2 ==0){
+           // input.setAttribute("value",i );
+            td.style.backgroundColor ="red";
             td.appendChild(input);
-            td.appendChild(input2);
+            td.style.height = "40px";
+            td.style.width = "40px";
+            }
+                if(j%2 == 1){
+              //  input2.setAttribute("value", x );
+              td.appendChild(input);
+                td.style.backgroundColor = "black";
+              
+                td.style.height = "40px";
+                td.style.width = "40px";
+            }}
+            if(i%2==1){
+                if(j%2 ==0){
+              //  input.setAttribute("value",i );
+                td.style.backgroundColor ="black";
+                td.appendChild(input);
+                td.style.height = "40px";
+                td.style.width = "40px";
+                }
+                if(j%2 == 1){
+               //     input2.setAttribute("value", x );
+                    td.style.backgroundColor = "red";
+                    td.appendChild(input2);
+                    td.style.height = "40px";
+                    td.style.width = "40px";
+                }
+            }
+          
             tr.appendChild(td); 
          //   tr.appendChild(td2); 
         }
         tablebody.appendChild(tr);
-
+     
         
     }
     document.getElementsByClassName("table")[0].appendChild(table);
